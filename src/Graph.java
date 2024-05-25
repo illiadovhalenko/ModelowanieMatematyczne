f7cee45183786cf5dfb06d74678d874135f8b183
illiadovhalenko <86498256+illiadovhalenko@users.noreply.github.com>
2024-05-25 09:59:33 +0200

    added new version

--- src/Graph.java
+++ src/Graph.java
@@ -29,9 +29,28 @@ public class Graph {
         }
         startPoint = map.get(0).get(0);
         Random random = new Random();
-        int randX = random.nextInt(n);
-        int randY = random.nextInt(n);
+//        int randX = random.nextInt(n);
+//        int randY = random.nextInt(n);
+        int randX = (int) (random.nextGaussian()*200+500);
+        int randY =  (int) (random.nextGaussian()*200+500);
+        if(randX>=1000)
+        {
+            randX=999;
+        }
+        if(randX<0)
+        {
+            randX=0;
+        }
+        if(randY>=1000)
+        {
+            randY=999;
+        }
+        if(randY<0)
+        {
+            randY=0;
+        }
         map.get(randY).get(randX).setFinalDestination();
+//        System.out.println(randY+" "+ randX);
     }
 
     @Override
@@ -51,25 +70,25 @@ public class Graph {
         boolean down = true;
         Node currentNode = startPoint;
 //        System.out.println(currentNode.getRight());
-        while (currentNode.getRight()!=null){
+        do{
             while((down ? currentNode.getDown() : currentNode.getUp())!= null){
-                System.out.println("0"+currentNode);
+//                System.out.println("0"+currentNode);
                 if(currentNode.isFinalDestination()){
-                    System.out.println("Found final destination"+ currentNode);
+//                    System.out.println("Found final destination"+ currentNode);
                     return numberOfSteps;
                 }
                 currentNode = (down ? currentNode.getDown() : currentNode.getUp());
                 numberOfSteps++;
             }
-            System.out.println("1"+currentNode);
+//            System.out.println("1"+currentNode);
             if(currentNode.isFinalDestination()){
-                System.out.println("Found final destination" + currentNode);
+//                System.out.println("Found final destination" + currentNode);
                 return numberOfSteps;
             }
             currentNode = currentNode.getRight();
             down = !down;
             numberOfSteps++;
-        }
+        }while (currentNode!=null);
         return -1;
     }
 
@@ -80,43 +99,98 @@ public class Graph {
      * 3 -- left
      * @return -- number of steps to reach finalDestination
      */
-//    public int secondMethod(){
-//        int numberOfSteps = 0;
-//        Node currentNode = startPoint;
-//        currentNode.setVisited();
-//        int direction = 0;
-//        while(!currentNode.isFinalDestination()){
-//            System.out.println("lalal");
-//        }
-//    }
-    public int randomDirectionMethod(){
+    public int secondMethod(){
+        int numberOfSteps = 0;
+        Node currentNode = startPoint;
+        int direction = 0;
+//        System.out.print(currentNode + " ");
+        if(currentNode.isFinalDestination()) return numberOfSteps;
+        while(!currentNode.isFinalDestination()){
+            switch (direction % 4){
+                case 0 -> {
+                    while(currentNode.getDown() != null && !currentNode.getDown().getVisited()){
+                        currentNode.setVisited();
+                        currentNode = currentNode.getDown();
+                        numberOfSteps++;
+//                        System.out.print(currentNode + " ");
+                        if(currentNode.isFinalDestination()){
+//                            System.out.print(numberOfSteps);
+                            return numberOfSteps;
+                        }
+                    }
+                    direction++;
+                }
+                case 1->{
+                    while(currentNode.getRight() != null && !currentNode.getRight().getVisited()){
+                        currentNode.setVisited();
+                        currentNode = currentNode.getRight();
+                        numberOfSteps++;
+//                        System.out.print(currentNode + " ");
+                        if(currentNode.isFinalDestination()){
+//                            System.out.print(numberOfSteps);
+                            return numberOfSteps;
+                        }
+                    }
+                    direction++;
+                }
+                case 2 ->{
+                    while(currentNode.getUp() != null && !currentNode.getUp().getVisited()){
+                        currentNode.setVisited();
+                        currentNode = currentNode.getUp();
+                        numberOfSteps++;
+//                        System.out.print(currentNode + " ");
+                        if(currentNode.isFinalDestination()){
+//                            System.out.print(numberOfSteps);
+                            return numberOfSteps;
+                        }
+                    }
+                    direction++;
+                }
+                case 3->{
+                    while(currentNode.getLeft() != null && !currentNode.getLeft().getVisited()){
+                        currentNode.setVisited();
+                        currentNode = currentNode.getLeft();
+                        numberOfSteps++;
+//                        System.out.print(currentNode + " ");
+                        if(currentNode.isFinalDestination()){
+//                            System.out.print(numberOfSteps);
+                            return numberOfSteps;
+                        }
+                    }
+                    direction++;
+                }
+            }
+        }
+        return numberOfSteps;
+    }
+    public int randomDirectionMethod() {
         int numberOfSteps = 0;
         Node currentNode = startPoint;
         int direction = 0;
         Random random = new Random();
-        while(!currentNode.isFinalDestination()){
+        while (!currentNode.isFinalDestination()) {
             direction = random.nextInt(4);
-            switch (direction){
+            switch (direction) {
                 case 0 -> {
-                    if(currentNode.getDown()!=null){
+                    if (currentNode.getDown() != null) {
                         currentNode = currentNode.getDown();
                         numberOfSteps++;
                     }
                 }
-                case 1->{
-                    if(currentNode.getRight()!=null){
+                case 1 -> {
+                    if (currentNode.getRight() != null) {
                         currentNode = currentNode.getRight();
                         numberOfSteps++;
                     }
                 }
-                case 2 ->{
-                    if(currentNode.getUp()!=null){
+                case 2 -> {
+                    if (currentNode.getUp() != null) {
                         currentNode = currentNode.getUp();
                         numberOfSteps++;
                     }
                 }
-                case 3->{
-                    if(currentNode.getLeft()!=null){
+                case 3 -> {
+                    if (currentNode.getLeft() != null) {
                         currentNode = currentNode.getLeft();
                         numberOfSteps++;
                     }
@@ -128,9 +202,232 @@ public class Graph {
 
     public int thirdMethod(){
         int numberOfSteps = 0;
+        Node currentNode = startPoint;
+        Node centerNode;
+        boolean odd = false;
 
-        return -1;
+        //depends on the size where the center is
+        if( n % 2 == 0) centerNode = map.get(n / 2 - 1).get(n / 2 - 1);
+        else{
+            centerNode = map.get(n / 2).get(n / 2);
+            odd = true;
+        }
+
+        //the path to our center  right|down
+        currentNode.setVisited();
+//        System.out.print(currentNode + " ");
+        if(currentNode.isFinalDestination()) return numberOfSteps;
+        while(currentNode != centerNode){
+
+            currentNode = currentNode.getRight();
+//            System.out.print(currentNode + " ");
+            currentNode.setVisited();
+            numberOfSteps++;
+            if(currentNode.isFinalDestination()) return numberOfSteps;
+
+            currentNode = currentNode.getDown();
+//            System.out.print(currentNode + " ");
+            currentNode.setVisited();
+            numberOfSteps++;
+            if(currentNode.isFinalDestination()) return numberOfSteps;
+        }
+
+        // now we have currentNode == centerNode
+//        System.out.print(currentNode + " ");
+        if(odd) {
+            for (int i = 1; i < n; i++) {
+                if (i == n - 1) {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getRight();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getUp();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getLeft();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                } else if (i % 2 != 0) {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getLeft();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getDown();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                } else {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getRight();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getUp();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                }
+            }
+        }
+        else{ //if even
+            for (int i = 1; i < n; i++) {
+                if (i == n - 1) {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getRight();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getUp();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getLeft();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                }
+                else if(i == 1){
+                    currentNode = currentNode.getRight();
+//                    System.out.print(currentNode + " ");
+                    if(!currentNode.getVisited()){
+                        currentNode.setVisited();
+                        numberOfSteps++;
+                    }
+                    if(currentNode.isFinalDestination()) return numberOfSteps;
+                    currentNode = currentNode.getDown();
+//                    System.out.print(currentNode + " ");
+                    if(!currentNode.getVisited()){
+                        currentNode.setVisited();
+                        numberOfSteps++;
+                    }
+                    if(currentNode.isFinalDestination()) return numberOfSteps;
+                    currentNode = currentNode.getLeft();
+//                    System.out.print(currentNode + " ");
+                    if(!currentNode.getVisited()){
+                        currentNode.setVisited();
+                        numberOfSteps++;
+                    }
+                    if(currentNode.isFinalDestination()) return numberOfSteps;
+                    currentNode = currentNode.getUp();
+//                    System.out.print(currentNode + " ");
+                    if(!currentNode.getVisited()){
+                        currentNode.setVisited();
+                        numberOfSteps++;
+                    }
+                    if(currentNode.isFinalDestination()) return numberOfSteps;
+                    currentNode = currentNode.getLeft();
+//                    System.out.print(currentNode + " ");
+                    if(!currentNode.getVisited()){
+                        currentNode.setVisited();
+                        numberOfSteps++;
+                    }
+                    if(currentNode.isFinalDestination()) return numberOfSteps;
+                }
+                else if(i == 2){
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getDown();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                }
+                else if (i % 2 == 0) {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getLeft();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getDown();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                } else {
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getRight();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                    for (int j = 1; j <= i; j++) {
+                        currentNode = currentNode.getUp();
+//                        System.out.print(currentNode + " ");
+                        if(!currentNode.getVisited()){
+                            currentNode.setVisited();
+                            numberOfSteps++;
+                        }
+                        if(currentNode.isFinalDestination()) return numberOfSteps;
+                    }
+                }
+            }
+        }
+        return numberOfSteps;
     }
+
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+
+    static Graph graph;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        graph = new Graph(5);
+//        System.out.println(graph);
+//        graph.firstMethod();
+//        System.out.println("1--------");
+//        graph.secondMethod();
+//        System.out.println("\n2-------");
+//        graph.thirdMethod();
+        String filePath = "output.txt";
+
+        try {
+            // Создание объекта FileWriter с указанием пути к файлу
+            FileWriter writer = new FileWriter(filePath);
+            // Запись строки в файл
+            for(int i=0; i<1000; i++){
+                graph = new Graph(1000);
+                writer.write(String.valueOf(graph.randomDirectionMethod())+"\n");
+            }
+            // Закрытие потока
+            writer.close();
+            System.out.println("Запись в файл успешно выполнена!");
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+
     }
 }
